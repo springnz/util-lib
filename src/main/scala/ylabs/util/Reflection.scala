871fad63e2bf11a48e1936bc680538d83197a8b7
Stephen Zoio <stephen@springdom.com>
2015-07-21 15:05:27 +1200

    Added methods to 1) create from Map, 2) get the list of field names

--- src/main/scala/ylabs/util/Reflection.scala
+++ src/main/scala/ylabs/util/Reflection.scala
@@ -6,11 +6,24 @@ import scala.reflect.runtime.universe._
 object Reflection {
 
   def createInstance[T: TypeTag](args: Any*): T = {
-    val tt = typeTag[T]
-    val ruType = tt.tpe
-    val constructorSymbol = ruType.members.filter(m => m.isMethod && m.asMethod.isConstructor).head.asMethod
+    val ruType = typeTag[T].tpe
+    val constructorSymbol = constructor[T]
+    val p: List[universe.Symbol] = constructorSymbol.paramLists(0)
     val constructorMethod = currentMirror.reflectClass(ruType.typeSymbol.asClass).reflectConstructor(constructorSymbol)
     constructorMethod(args: _*).asInstanceOf[T]
   }
+
+  def createInstanceFromMap[T: TypeTag](argMap: Map[String, _]): T = {println(typeOf[T].members)
+    val arguments = getFieldNames map argMap toSeq
+    val instance = createInstance(arguments: _*)
+    instance
+  }
+
+  def getFieldNames[T: TypeTag]: Iterable[String] = constructor.paramLists(0).map (a => a.name.toString)
+
+  private def constructor[T: TypeTag](): universe.MethodSymbol = typeTag[T].tpe.members
+    .filter { m => m.isMethod && m.asMethod.isConstructor }
+    .head.asMethod
+
 }
 
--- src/test/scala/ylabs/util/ReflectionTest.scala
+++ src/test/scala/ylabs/util/ReflectionTest.scala
@@ -2,20 +2,33 @@ package ylabs.util
 
 import org.scalatest._
 
-case class Person(name: String, age: Int)
+case class Person(name: String, age: Int, email: String)
 
 class ReflectionTest extends WordSpec with ShouldMatchers {
   "Reflection" should {
     "constructs a Person object" in {
-      val person = Reflection.createInstance[Person]("Bob", 42)
-      person shouldBe Person("Bob", 42)
+      val person = Reflection.createInstance[Person]("Bob", 42, "bob@spring.nz")
+      person shouldBe Person("Bob", 42, "bob@spring.nz")
     }
   }
   it should {
     "works for a sequence" in {
-      val args = Seq("Bob", 42)
+      val args = Seq("Bob", 42, "bob@spring.nz")
       val person = Reflection.createInstance[Person](args: _*)
-      person shouldBe Person("Bob", 42)
+      person shouldBe Person("Bob", 42, "bob@spring.nz")
+    }
+  }
+  it should {
+    "works for a map" in {
+      val argMap = Map("name" -> "Bob", "age" -> 42, "email" -> "bob@spring.nz")
+      val person = Reflection.createInstanceFromMap[Person](argMap)
+      person shouldBe Person("Bob", 42, "bob@spring.nz")
+    }
+  }
+  it should {
+    "get the constructor arguments / fields of a case class" in {
+      val fields = Reflection.getFieldNames[Person]
+      fields.toList shouldBe List("name", "age", "email")
     }
   }
 }
