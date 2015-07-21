2035bb2b482ad701aa5fe23d264ad3b3b885c5c2
Stephen Zoio <stephen@springdom.com>
2015-07-21 17:07:58 +1200

    removed println from map

--- src/main/scala/ylabs/util/Reflection.scala
+++ src/main/scala/ylabs/util/Reflection.scala
@@ -8,18 +8,17 @@ object Reflection {
   def createInstance[T: TypeTag](args: Any*): T = {
     val ruType = typeTag[T].tpe
     val constructorSymbol = constructor[T]
-    val p: List[universe.Symbol] = constructorSymbol.paramLists(0)
     val constructorMethod = currentMirror.reflectClass(ruType.typeSymbol.asClass).reflectConstructor(constructorSymbol)
     constructorMethod(args: _*).asInstanceOf[T]
   }
 
-  def createInstanceFromMap[T: TypeTag](argMap: Map[String, _]): T = {println(typeOf[T].members)
+  def createInstanceFromMap[T: TypeTag](argMap: Map[String, _]): T = {
     val arguments = getFieldNames map argMap toSeq
     val instance = createInstance(arguments: _*)
     instance
   }
 
-  def getFieldNames[T: TypeTag]: Iterable[String] = constructor.paramLists(0).map (a => a.name.toString)
+  def getFieldNames[T: TypeTag]: Iterable[String] = constructor.paramLists(0).map(a => a.name.toString)
 
   private def constructor[T: TypeTag](): universe.MethodSymbol = typeTag[T].tpe.members
     .filter { m => m.isMethod && m.asMethod.isConstructor }
