b9d7da22728b3236baec5603c149fe9c9db211fe
Stephen Zoio <stephen@springdom.com>
2015-08-11 11:42:53 +1200

    Took s's out of test description. Improved Exception tests

--- src/test/scala/ylabs/util/ReflectionTest.scala
+++ src/test/scala/ylabs/util/ReflectionTest.scala
@@ -6,20 +6,20 @@ case class Person(name: String, age: Int, email: String)
 
 class ReflectionTest extends WordSpec with ShouldMatchers {
   "Reflection" should {
-    "constructs a Person object" in {
+    "construct a Person object" in {
       val person = Reflection[Person].createInstance("Bob", 42, "bob@spring.nz")
       person.get shouldBe Person("Bob", 42, "bob@spring.nz")
     }
   }
   it should {
-    "works for a sequence" in {
+    "work for a sequence" in {
       val args = Seq("Bob", 42, "bob@spring.nz")
       val person = Reflection[Person].createInstance(args: _*)
       person.get shouldBe Person("Bob", 42, "bob@spring.nz")
     }
   }
   it should {
-    "works for a map" in {
+    "work for a map" in {
       val argMap = Map("name" -> "Bob", "age" -> 42, "email" -> "bob@spring.nz")
       val person = Reflection[Person].createInstanceFromMap(argMap)
       person.get shouldBe Person("Bob", 42, "bob@spring.nz")
@@ -32,30 +32,30 @@ class ReflectionTest extends WordSpec with ShouldMatchers {
     }
   }
   it should {
-    "throws an exception if the wrong key is used" in {
-      an[NoSuchElementException] should be thrownBy {
-        val argMap = Map("namewrong" -> "Bob", "age" -> 42, "email" -> "bob@spring.nz")
+    "throw an exception if the wrong key is used" in {
+      val argMap = Map("namewrong" -> "Bob", "age" -> 42, "email" -> "bob@spring.nz")
+      intercept[NoSuchElementException] {
         Reflection[Person].createInstanceFromMap(argMap).get
       }
     }
   }
   it should {
-    "throws an exception if called with wrong types" in {
-      an[IllegalArgumentException] should be thrownBy {
+    "throw an exception if called with wrong types" in {
+      intercept[IllegalArgumentException] {
         val person = Reflection[Person].createInstance("Bob", "42", "bob@spring.nz").get
       }
     }
   }
   it should {
-    "throws an exception if called with too few parameters" in {
-      an[IllegalArgumentException] should be thrownBy {
+    "throw an exception if called with too few parameters" in {
+      intercept[IllegalArgumentException] {
         val person = Reflection[Person].createInstance("Bob", 42).get
       }
     }
   }
   it should {
-    "throws an exception if called with too many parameters" in {
-      an[IllegalArgumentException] should be thrownBy {
+    "throw an exception if called with too many parameters" in {
+      intercept[IllegalArgumentException] {
         val person = Reflection[Person].createInstance("Bob", 42, "bob@spring.nz", "TooMany").get
       }
     }
