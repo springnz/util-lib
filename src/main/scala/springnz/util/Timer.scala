86b9e2f14c345a2030b9b3e28ffa4b8449e20375
Mike Ourednik <mike@spring.co.nz>
2016-01-26 13:26:59 +1300

    Updated timer to use nanoTime() instead of currentTimeMillis()

--- src/main/scala/springnz/util/Timer.scala
+++ src/main/scala/springnz/util/Timer.scala
@@ -2,27 +2,27 @@ package springnz.util
 
 import com.typesafe.scalalogging.Logger
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 object Timer extends Logging {
   def timed[A](processName: Option[String] = None)(execution: ⇒ A): A = {
-    val start = System.currentTimeMillis()
+    val start = System.nanoTime()
     val result = execution
-    val time = System.currentTimeMillis() - start
+    val timeMs = (System.nanoTime() - start) / 1000000
     val messageStart = processName.getOrElse(s"'${result.toString}'")
-    log.info(s"$messageStart execution time: ${time}ms")
+    log.info(s"$messageStart execution time: ${timeMs}ms")
     result
   }
 
-  implicit class LazyFuturePimper[T](f: => Future[T]) {
+  implicit class LazyFuturePimper[T](f: ⇒ Future[T]) {
     def timed(processName: Option[String] = None)(implicit log: Logger, ec: ExecutionContext): Future[T] = {
-      val start = System.currentTimeMillis()
+      val start = System.nanoTime()
       f.zip(Future { start })
         .map {
           case (result, start) ⇒
-            val time = System.currentTimeMillis() - start
+            val timeMs = (System.nanoTime() - start) / 1000000
             val messageStart = processName.getOrElse(s"'${result.toString}'")
-            log.info(s"$messageStart execution time: ${time}ms")
+            log.info(s"$messageStart execution time: ${timeMs}ms")
             result
         }
     }
