a751a8bad7524a4ae1cdcc4883121cf26654b929
Mike Ourednik <mike@spring.co.nz>
2016-03-01 14:05:09 +1300

    Fixed test

--- src/test/scala/springnz/util/IOUtilTest.scala
+++ src/test/scala/springnz/util/IOUtilTest.scala
@@ -7,7 +7,7 @@ class IOUtilTest extends WordSpec with ShouldMatchers {
   "IOUtil" should {
     "create tmp file, write and read file" in {
       val tmpFile = IOUtil.createTempFile()
-      val tmpFileName = tmpFile.getFileName.toString
+      val tmpFileName = tmpFile.toString
       val fileContent = Seq("line1", "line2")
 
       IOUtil.writeTextFile(tmpFileName, fileContent.mkString("\n"))
