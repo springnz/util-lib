c42599b989620bf27cc829768a4a8d09cb4c47ae
Mike Ourednik <mike@spring.co.nz>
2016-04-08 14:34:27 +1200

    improved config environment

--- src/main/scala/springnz/util/ConfigEnvironment.scala
+++ src/main/scala/springnz/util/ConfigEnvironment.scala
@@ -2,13 +2,36 @@ package springnz.util
 
 import com.typesafe.config.ConfigFactory
 
+import scala.util.Try
+
 object ConfigEnvironment extends Logging {
 
   val config = {
     val rootConfig = ConfigFactory.load()
-    val deploymentEnvironment = rootConfig.getString("deployment-environment")
-    log.info(s"Using $deploymentEnvironment config")
-    rootConfig.getConfig(deploymentEnvironment)
-  }
+    val sharedPrefix: Option[String] = Try { rootConfig.getString("config-shared-prefix") }.toOption
+    val environmentPrefix: Option[String] = Try { rootConfig.getString("config-environment-prefix") }.toOption
 
+    sharedPrefix.foreach { prefix ⇒
+      log.info(s"Using shared config prefix [$prefix]")
+    }
+
+    environmentPrefix.foreach { prefix ⇒
+      log.info(s"Using environment config prefix [$prefix]")
+    }
+
+    val optionalSharedConfig = sharedPrefix.map(rootConfig.getConfig)
+    val optionalEnvironmentConfig = environmentPrefix.map(rootConfig.getConfig)
+
+    (optionalEnvironmentConfig, optionalSharedConfig) match {
+      case (Some(environmentConfig), Some(sharedConfig)) ⇒
+        environmentConfig.withFallback(sharedConfig)
+      case (Some(environmentConfig), None) ⇒
+        environmentConfig
+      case (None, Some(sharedConfig)) ⇒
+        sharedConfig
+      case _ ⇒
+        log.info("Using root config")
+        ConfigFactory.load()
+    }
+  }
 }
